f55853358611f443acacedb0c91aaef8b722d15b
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-01-09 14:18:12 -0600

    category page

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Home } from './pages/Home';
 import { Cart } from './pages/Cart';
 import SingleProduct from './pages/SingleProduct';
+import { ProductList } from './pages/ProductList';
 
 const App = () => {
   return (
     <>
       {/* <Home />  */}
       {/* <Cart /> */}
-      <SingleProduct />
+      {/* <SingleProduct /> */}
+      <ProductList />
     </>
   )
 }
--- src/pages/ProductList.tsx
+++ src/pages/ProductList.tsx
@@ -1,9 +1,48 @@
+import { Navbar } from '../components/Navbar';
+import { Newsletter } from '../components/Newsletter';
+import { Products } from '../components/Products';
+import { Footer } from '../components/Shared/Footer';
 
 
 export const ProductList = () => {
     return (
         <>
-            
+            <div className="categoryContainer">
+                <Navbar />
+                <h4 className='title'>Football</h4>
+                <div className="filterContainer">
+                    <div className="filter">
+                        <p className="filterText">Buscar por: </p>
+                        <select className='filterSelect' name="" id="">
+                            <option value="" disabled selected>Color</option>
+                            <option value="Blanco">Blanco</option>
+                            <option value="Negro">Negro</option>
+                            <option value="Rojo">Rojo</option>
+                            <option value="Azul">Azul</option>
+                            <option value="Verde">Verde</option>
+                        </select>
+                        <select className='filterSelect' name="" id="">
+                            <option value="" disabled selected>Tamaño</option>
+                            <option value="">XS</option>
+                            <option value="">S</option>
+                            <option value="">M</option>
+                            <option value="">L</option>
+                            <option value="">XL</option>
+                        </select>
+                    </div>
+                    <div className="filter">
+                        <p className="filterText">Ordenar por: </p>
+                        <select className='filterSelect' name="" id="">
+                            <option value="" disabled selected>Más nuevo</option>
+                            <option value="Blanco">Precio (ASC)</option>
+                            <option value="Negro">Precio (DSC)</option>
+                        </select>
+                    </div>
+                </div>
+                <Products />
+                <Newsletter />
+                <Footer />
+            </div>
         </>
     )
 }
