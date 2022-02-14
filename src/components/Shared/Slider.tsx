80ab5a50ca77919579945dfec8c4e414d25662b7
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-02-14 11:20:46 -0600

    changed price background-color

--- src/components/Shared/Slider.tsx
+++ src/components/Shared/Slider.tsx
@@ -1,5 +1,11 @@
+import { RootStateOrAny, useSelector } from 'react-redux';
+import Skeleton from 'react-loading-skeleton'
 
 export const Slider = () => {
+
+    const { loading } = useSelector((state: RootStateOrAny) => state.ui);
+    console.log(loading);
+
     return (
         <div className='container full-height animate__animated animate__fadeIn animate__faster'>
             <div className="slide">
@@ -10,10 +16,23 @@ export const Slider = () => {
                         <p className='slideButton'>LEARN MORE ...</p>
                     </div>
                 </div>
-
-                <div className="imageContainerSlider">
-                    <img className='image' src="https://res.cloudinary.com/dhyxqmnua/image/upload/v1641613241/Olympus/pexels-pixabay-220383_utmdhd.jpg" alt="" />
-                </div>
+                {
+                    loading ?
+                        (
+                            <Skeleton
+                                circle
+                                height="400px"
+                                width="400px"
+                                containerClassName="avatar-skeleton"
+                            />
+                        )
+                        :
+                        (
+                            <div className="imageContainerSlider">
+                                <img className='image' src="https://res.cloudinary.com/dhyxqmnua/image/upload/v1641613241/Olympus/pexels-pixabay-220383_utmdhd.jpg" alt="" />
+                            </div>
+                        )
+                }
             </div>
 
         </div>
