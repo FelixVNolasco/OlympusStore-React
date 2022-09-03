3621661f7077edcf961bee0e9a27b5cf1cffec4d
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-09-03 16:00:56 -0500

    get localeDate in the profile Section

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,21 +2,30 @@ import { RootStateOrAny, useSelector } from "react-redux";
 import { Navbar } from '../components/Navbar';
 import { useForm } from '../hooks/useForm';
 
- const Profile = () => {
+const Profile = () => {
 
     const user = useSelector((state: RootStateOrAny) => state.user.currentUser);
 
-    const { displayName, photoURL, email  } = user;
+    const { displayName, photoURL, email, creationTime, lastSignInTime } = user;
+    const CreationDate = new Date(creationTime);
+    const lastSignInTimeDate = new Date(lastSignInTime);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' } as const;
+    
+    const CreationDateParsed = CreationDate.toLocaleDateString("es-Mx", options);
+    const lastSignInTimeDateParsed = lastSignInTimeDate.toLocaleDateString("es-Mx", options);
 
     const [formValues, handleInputChange] = useForm({
         name: displayName,
         userEmail: email
     });
 
-    const {name, userEmail} = formValues;
+    const { name, userEmail } = formValues;
+
+
+    console.log(user);
 
     return <>
-        <div className="container"> 
+        <div className="container">
             <Navbar />
             <div className="profileContainer animate__animated animate__fadeIn animate__faster">
                 <div className="profileSection">
@@ -24,39 +33,40 @@ import { useForm } from '../hooks/useForm';
                         <img className="img" src={(photoURL) ? photoURL : "https://res.cloudinary.com/dhyxqmnua/image/upload/v1642722284/Olympus/blank-profile-picture-973460_qb0gmg.svg"} alt="" />
                     </div>
 
-                    <div className="infoContainer">
-                        <p className="info">Nombre:</p>
-                        <input className="userDataInput" type="text"  value={name} onChange={handleInputChange}/>
-                    </div>
+                    <div className="profileGrid">
 
-                    <div className="infoContainer">
-                        <p className="info">Correo Electrónico:</p>
-                        <input className="userDataInput" type="text"  value={userEmail} onChange={handleInputChange}/>
-                    </div>
-
-                    <div className="updateContainer">
-                        <div className="updateAccountBtn">
-                            Actualizar Información
+                        <div className="infoContainer">
+                            <p className="info">Nombre:</p>
+                            <input className="userDataInput" type="text" value={name} onChange={handleInputChange} />
                         </div>
-                    </div>
 
-                    <div className="deleteContainer">
-                        <div className="deleteAccountBtn">
-                            Eliminar Cuenta
+                        <div className="infoContainer">
+                            <p className="info">Correo Electrónico:</p>
+                            <input className="userDataInput" type="text" value={userEmail} onChange={handleInputChange} />
                         </div>
-                    </div>
-
-                    <div className="lastInfoContainer">
+                        
                         <div className="createdAt">
                             <p className="info">Cuenta creada en:</p>
-                            <p className="profileDate">{user.creationTime}</p>
+                            <p className="profileDate">{CreationDateParsed}</p>
                         </div>
+
                         <div className="updatedAt">
                             <p className="info">Último inicio de sesión:</p>
-                            <p className="profileDate">{user.lastSignInTime}</p>
+                            <p className="profileDate">{lastSignInTimeDateParsed}</p>
                         </div>
-                    </div>
 
+                        <div className="updateContainer">
+                            <button disabled={true} className="updateAccountBtn">
+                                Actualizar Información
+                            </button>
+                        </div>
+
+                        <div className="deleteContainer">
+                            <button disabled={true} className="deleteAccountBtn">
+                                Eliminar Cuenta
+                            </button>
+                        </div>                        
+                    </div>
                 </div>
             </div>
         </div>
