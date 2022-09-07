be7a42853cc8a4d584c37e5f3bc49731ff5e6da6
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-09-07 18:46:34 -0500

    updated provisional token

--- src/requestMethods.tsx
+++ src/requestMethods.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = "https://olympus-backend.vercel.app/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZGVmZTU1ZDc5NDMzYTEyOWEzM2E5MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjMyNTc3MCwiZXhwIjoxNjYyNTg0OTcwfQ.F-Dg3-pyDMiEjIVAnKIm9xHXD5BGTPJb7Se5Zd2IEIo"
+const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZGVmZTU1ZDc5NDMzYTEyOWEzM2E5MyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MjU5NDM3NiwiZXhwIjoxNjYyODUzNTc2fQ.DBt0v9_llhbmF4cYCkRz5jbxsrMbVTcmIPJJe35Xlyc"
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
