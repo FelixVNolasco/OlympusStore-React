7f1c0683c45cd84e6fef4f3956b828eecb983fe4
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-09-30 15:26:56 -0500

    added protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,8 @@ import Login from "./components/Auth/Login";
 import Signup from "./components/Auth/Signup";
 import { NavbarComponent } from "./components/Shared/Navbar/NavbarComponent";
 import { UpdateProfile } from "./pages/UpdateProfile";
-import { BallTriangle } from "react-loader-spinner";
+import {RootStateOrAny, useSelector} from "react-redux";
+import {ProtectedRoute, ProtectedRouteProps} from "./components/ProtectedRoute";
 
 //Pages
 const ProductList = React.lazy(() => import('./pages/ProductList'));
@@ -17,11 +18,30 @@ const Success = React.lazy(() => import('./pages/Success'));
 const Profile = React.lazy(() => import('./pages/Profile'));
 
 const App = () => {
+
+  const user = useSelector((state: RootStateOrAny) => state.user.currentUser);
+
+  const defaultProtectedRouteProps: Omit<ProtectedRouteProps, 'outlet'> = {
+    isAuthenticated: user,
+    authenticationPath: '/auth/login',
+  };
+
   return (
     <BrowserRouter>
       <NavbarComponent />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth/login" element={
+          <Login />
+        } />
+        <Route path="/auth/signup" element={
+          <Signup />
+        } />
+        <Route path="/restore-password" element={
+          <React.Suspense fallback={<></>}>
+            <RestorePassword />
+          </React.Suspense>
+        } />
         <Route path="/products/:category" element={
           <React.Suspense fallback={<></>}>
             <ProductList />
@@ -32,39 +52,30 @@ const App = () => {
             <SingleProduct />
           </React.Suspense>
         } />
-        <Route path="/cart" element={
-          <React.Suspense fallback={<></>}>
-            <Cart />
-          </React.Suspense>
-        } />
-        <Route path="/auth/login" element={
-          <Login />
-        } />
-        <Route path="/auth/signup" element={
-          <Signup />
-        } />
-        <Route path="/success" element={
-          <React.Suspense fallback={<></>}>
-            <Success />
-          </React.Suspense>
-        } />
         <Route path="/profile" element={
           <React.Suspense fallback={<></>}>
-            <Profile />
+            <ProtectedRoute {...defaultProtectedRouteProps} outlet={<Profile />} />
+          </React.Suspense>
+        } />
+        <Route path="/profile/updateProfile/:id" element={
+          <React.Suspense fallback={<></>}>
+            <ProtectedRoute {...defaultProtectedRouteProps} outlet={<UpdateProfile />} />
+          </React.Suspense>
+        } />
+        <Route path="/cart" element={
+          <React.Suspense fallback={<></>}>
+            <ProtectedRoute {...defaultProtectedRouteProps} outlet={<Cart />} />
           </React.Suspense>
         } />
         <Route path="/purchases" element={
           <React.Suspense fallback={<></>}>
-            <Purchases />
+            <ProtectedRoute {...defaultProtectedRouteProps} outlet={<Purchases />} />
           </React.Suspense>
         } />
-        <Route path="/restore-password" element={
+        <Route path="/success" element={
           <React.Suspense fallback={<></>}>
-            <RestorePassword />
+            <ProtectedRoute {...defaultProtectedRouteProps} outlet={<Success />} />
           </React.Suspense>
-        } />
-        <Route path="/profile/updateProfile/:id" element={
-          <UpdateProfile />
         } />
       </Routes>
     </BrowserRouter>
---
+++ src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import {Navigate} from "react-router-dom";
+
+export type ProtectedRouteProps = {
+    isAuthenticated: any;
+    authenticationPath: string;
+    outlet: JSX.Element;
+};
+
+export const ProtectedRoute = ({isAuthenticated, authenticationPath, outlet}: ProtectedRouteProps) => {
+    console.log(isAuthenticated);
+    if(isAuthenticated._id) {
+        return outlet;
+    } else {
+        return <Navigate to={{ pathname: authenticationPath }} replace />;
+    }
+};
