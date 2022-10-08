34b4d9f2eaef653de3802e879d23652444a20a5e
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-10-08 13:02:21 -0500

    f

--- src/components/Shared/CheckoutButton.tsx
+++ src/components/Shared/CheckoutButton.tsx
@@ -6,22 +6,31 @@ export const CheckoutButton = ({ items }) => {
 
     const { _id } = useSelector((state: RootStateOrAny) => state.user.currentUser);
 
-    const handleCheckout = () => {
+    const handleCheckout = async () => {
 
         // userRequest.post("/stripe/create-checkout-session", { items, userId: _id }).then((response) => {
         //     console.log(response)
         // }).catch((error) => console.log(error.message));
-        axios
-            .post("https://olympus-backend.vercel.app/api/stripe/create-checkout-session", {
-                items,
-                userId: _id,
-            })
-            .then((response) => {
-                if (response.data.url) {
-                    window.location.href = response.data.url;
-                }
-            })
-            .catch((err) => console.log(err.message));
+        // axios
+        //     .post("https://olympus-backend.vercel.app/api/stripe/create-checkout-session", {
+        //         items,
+        //         userId: _id,
+        //     })
+        //     .then((response) => {
+        //         if (response.data.url) {
+        //             window.location.href = response.data.url;
+        //         }
+        //     })
+        //     .catch((err) => console.log(err.message));
+
+        const res = await fetch(`https://olympus-backend.vercel.app/api/stripe/create-checkout-session`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json'
+            }
+        })
+        const body = await res.json()
+        window.location.href = body.url
     };
     return (
         <>
