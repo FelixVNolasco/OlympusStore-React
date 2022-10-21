0af4dc30a35a75b0cc46451a17e953e8b186db75
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-10-21 18:06:29 -0500

    changed the slider component to hero

--- src/components/Shared/Hero.tsx
+++ src/components/Shared/Hero.tsx
@@ -1,14 +1,14 @@
 import { RootStateOrAny, useSelector } from 'react-redux';
 import Skeleton from 'react-loading-skeleton'
 
-export const Slider = () => {
+export const Hero = () => {
 
     const { loading } = useSelector((state: RootStateOrAny) => state.ui);
 
     return (
-        <section className='container mx-auto full-height text-gray-800'>
+        <section className='flex container mx-auto lg:mt-32 xl:mt-64 mb-64'>
             <div className='grid lg:grid-cols-2 px-4 md:px-24 items-center gap-4 lg:gap-12'>
-                <div className='mt-12 lg:mt-0'>
+                <div className='mt-12 lg:mt-0 text-gray-800'>
                     <h1 className='text-center lg:text-left text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight lg:mb-12'>Olympus Store
                         <br />
                         <span className='text-blue-600'>Los mejores en el campo</span>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 import Categories from '../components/Categories'
-import { Slider } from '../components/Shared/Slider'
+import { Hero } from '../components/Shared/Hero'
 import { Products } from '../components/Products';
 import { Footer } from '../components/Shared/Footer';
 
 export const Home = () => {
     return (
         <div className="home">    
-            <Slider />
+            <Hero />
             <Categories />
             <Products />
             <Footer />
