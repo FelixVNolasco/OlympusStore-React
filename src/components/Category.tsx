b4a7eb939e14d2f533b6c7325569efbc90670efe
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-11-20 15:58:48 -0600

    improved category img

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -4,7 +4,7 @@ const Category = ({ item }: any) => {
     return (
         <Link className='drop-shadow-lg hover:drop-shadow-2xl hover:-translate-y-2 transition ease-in-out duration-300' to={`/products/${item.category}`}>
             <div className=''>
-                <img className='w-11/12 rounded-lg' src={item.img} alt="" />
+                <img className='w-full lg:w-11/12 rounded-lg' src={item.img} alt="" />
             </div>
         </Link>
     )
